d2427a73c00bd78f78d63207e8d950ff347d3678
Jerrod <jlankford@salesforce.com>
2019-11-11 20:57:03 -0500

    now backed by mongodb

--- server/index.js
+++ server/index.js
@@ -1,37 +1,55 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
 const { ACTIONS } = require("../common/slack-blocks.js");
 
-let orders = [];
-
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/wingy";
+const PORT = process.env.PORT || 3000;
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.post("/slack", urlencodedParser, (req, res) => {
-  res.status(200).end();
+let orders;
+
+app.post("/slack", urlencodedParser, async (req, res) => {
   var actionJSONPayload = JSON.parse(req.body.payload);
-  parseAction(actionJSONPayload);
+  await parseAction(actionJSONPayload);
+  res.status(200).end();
 });
 
-app.get("/api/orders", (req, res) => {
-  res.json(orders);
+app.get("/api/orders", async (req, res) => {
+  const cursor = orders.find({});
+  const allOrders = await cursor.toArray();
+  res.status(200).send(allOrders).end();
 });
 
 app.post("/api/clear", (req, res) => {
-  orders = [];
-  res.status(200).end();
+  const result = orders.remove({});
+  if (!result.writeError) {
+    res.status(200).end();
+  } else {
+    res.status(400).send({error: result.writeError.errmsg}).end();
+  }
 });
 
-console.log(`Listening on ${PORT}...`);
-app.listen(PORT);
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  const dbo = db.db("wingy");
+  dbo.createCollection("orders", function(err, res) {
+    if (err) throw err;
+    orders = res;
+    console.log(`Listening on ${PORT}...`);
+    app.listen(PORT);
+  });
+});
 
-function parseAction(payload) {
+/**** Helper functions ******/
+async function parseAction(payload) {
   const user = payload.user;
   const action = payload.actions[0];
-  const order = lookupOrCreateOrder(user);
+  const order = await lookupOrCreateOrder(user);
 
+  let text;
   // In case it was complete before, any action will cause it to be uncompleted
   order.complete = false;
   switch (action.action_id) {
@@ -48,28 +66,58 @@ function parseAction(payload) {
       order.fries = parseSelect(action);
       break;
     case ACTIONS.ORDER:
-      const text = validateOrder(order);
-      sendMessage(payload.response_url, text);
+      text = validateOrder(order);
       break;
     default:
-      console.log(`Unknown action id ${action}`);
+      throw new Error(`Unknown action id ${action}`);
+  }
+
+  await updateOrder(order);
+
+  if (text) {
+    sendMessage(payload.response_url, text);
   }
 }
 
+// Lookup order from mongo or insert a new order
 function lookupOrCreateOrder(user) {
-  let order = orders.find(o => o.name === user.username);
-  if (order) {
-    return order;
-  }
+  return new Promise((resolve, reject) => {
+    orders.findOne({ name: user.username }, function(err, result) {
+      if (err) reject(error);
 
-  const date = new Date();
-  order = {
-    name: user.username,
-    user_id: user.id,
-    time: date.toLocaleString()
-  };
-  orders.push(order);
-  return order;
+      if (result) {
+        resolve(result);
+      } else {
+        // Create a new order with the given timestamp and user
+        const date = new Date();
+        const order = {
+          name: user.username,
+          user_id: user.id,
+          time: date.toLocaleString()
+        };
+
+        // Insert the new order and return it
+        orders.insertOne(order, function(err, res) {
+          if (err) reject(err);
+          console.log("new order created");
+          resolve(order);
+        });
+      }
+    });
+  });
+}
+
+// Update order in mongodb
+function updateOrder(order) {
+  return new Promise((resolve, reject) => {
+    var query = { name: order.name };
+    var values = { $set: {...order}};
+    orders.updateOne(query, values, function(err) {
+      if (err) reject(err);
+      console.log("order updated");
+      resolve(true);
+    });
+  });
 }
 
 // Form return values are Type:Size:Price eg (Boneless:DC-3:6.99)
