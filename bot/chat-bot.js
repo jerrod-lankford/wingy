85670f1286a3d70df6aeab8b2f2a6b77120ab9e9
Jerrod Lankford <jllankfo@ncsu.edu>
2021-08-19 11:39:55 -0400

    Attempt to fix receipt screenshot

--- bot/chat-bot.js
+++ bot/chat-bot.js
@@ -2,9 +2,7 @@ const secret = require('./secret.json');
 const { WebClient } = require('@slack/web-api');
 const { createReadStream } = require('fs');
 const { slackBlocks } = require('../common/slack-blocks.js');
-const chalk = require('chalk');
 const CONFIG = require('./configuration.json');
-const { timeout } = require('./utils');
 
 const RECEIPT_SELECTOR = '//div[contains(concat(" ",normalize-space(@class)," ")," purchase-confirmation ")]//img[@alt="confirmation Image"]/following-sibling::div';
 const ESTIMATED_DELIVERY_SELECTOR = 'div.purchase-confirmation [data-value="title1_accentDark"]';
@@ -34,12 +32,11 @@ module.exports.ChatBot = class ChatBot {
   }
 
   async postReceipt(page) {
-    await page.waitForSelector(RECEIPT_SELECTOR);
+    await page.waitForXPath(RECEIPT_SELECTOR);
+    const elements = await page.$x(RECEIPT_SELECTOR);
+    const receipt = elements[0];
 
-    // There seems to be a glitch that causes the receipt to be blank
-    // so lets wait a sec before taking the screenshot after finding the dom node
-    await timeout(1000);
-    await screenshotDOMElement(page, RECEIPT_SELECTOR, 'receipt.png');
+    await receipt.screenshot({path: 'receipt.png'});
 
     await this.web.files.upload({
       filename: 'receipt',
@@ -91,24 +88,6 @@ async function postMessage(web, additionalParams) {
 
   params = Object.assign(params, additionalParams);
   return await web.chat.postMessage(params);
-}
-
-async function screenshotDOMElement(page, selector, name) {
-  const rect = await page.evaluate(selector => {
-    const element = document.querySelector(selector);
-    const { x, y, width, height } = element.getBoundingClientRect();
-    return { left: x, top: y, width, height, id: element.id };
-  }, selector);
-
-  return await page.screenshot({
-    path: name,
-    clip: {
-      x: rect.left,
-      y: rect.top,
-      width: rect.width,
-      height: rect.height
-    }
-  });
 }
 
 function format(value) {
